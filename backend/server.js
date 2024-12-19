e5dbfe23432a989d8b2e8754b6c2d85a214704df
Htoo23 <htooaunglin2342002@gmail.com>
2024-12-19 16:45:47 +0630

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,41 @@
-const express=require("express");
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+// import authRoutes from "./routes/authRoutes.js";
+// import messageRoutes from "./routes/messageRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
 
-const app=express();
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app,io,server} from "./socket/socket.js";
 
-const PORT=process.env.PORT||5000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!!");
+dotenv.config();
+
+const _dirname=path.resolve();
+const PORT=process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/app/auth",authRoutes);
+app.use("/app/message",messageRoutes);
+app.use("/api/user",userRoutes);
+
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT,()=>console.log('Server Running on port $( '));
+server.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);   
+});
+
+
+
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,5 +12,29 @@ const io=new Server(server,{
 		origin: ["http://localhost:3000"],
 		methods: ["GET", "POST"],
 	},
-})
+});
+export const getReceiverSocketId=(receiverId)=>{
+	return userSocketMap[receiverId];
+};
+
+const userSocketMap={};
+
+io.on("connection",(socket)=>{
+	console,log("a user connected",socket.id);
+
+	const userId=socket.handshake.query.userId;
+	if(userId != "undefined")userSocketMap[userId]=socket.id;
+
+
+	io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+	socket.on("disconnect",()=>{
+		console.log("user disconnected",socket.id);
+		delete userSocketMap[userId];
+		io.emit("getOnlineUsers",Object.keys(userSocketMap));
+	});
+});
+export {app,io,server};
+
+
 
