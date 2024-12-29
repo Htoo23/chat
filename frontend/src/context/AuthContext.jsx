b710ecfa4fd734dbe2a7d90da08ddc5bbcdc5fb2
Htoo23 <htooaunglin2342002@gmail.com>
2024-12-29 20:01:24 +0630

    updated

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,12 @@
+import { createContext,useContext,useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext=()=>{
+    return useContext(AuthContext);
+};
+export const AuthProvider = ({children})=>{
+    const [authUser,setUser] = useState(JSON.parse(localStorage.getItem("chat-user"))||null);
+
+    return <AuthContext.Provider value={{authUser,setUser}}>{children}</AuthContext.Provider>
+}
